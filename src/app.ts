e1b19a4e7bd9fd8c6b21a9805373a2fd7467436e
jabin-v <jabinv03@gmail.com>
2024-04-04 12:02:33 +0530

    initail commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from "express";
+import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World!");
+});
+
+
+app.use(globalErrorHandler);
+
+export default app;
