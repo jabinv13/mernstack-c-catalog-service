fcc82cfbe2ed36543d1202bc83372d82b01cb1e2
jabin-v <jabinv03@gmail.com>
2024-04-10 23:13:21 +0530

    add create category endpoint

--- src/category/category-router.ts
+++ src/category/category-router.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import { CategoryController } from "./category-controller";
 import categoryValidator from "./category-validator";
+import { CategoryService } from "./category-service";
+import logger from "../config/logger";
 
 const router = express.Router();
 
-const categoryController = new CategoryController();
+const categorySerivce = new CategoryService();
+
+const categoryController = new CategoryController(categorySerivce, logger);
 
 router.post("/", categoryValidator, categoryController.create);
 
