beb457af8a8dd1da9c47aef1fe65f844350896e2
jabin-v <jabinv03@gmail.com>
2024-04-11 11:16:58 +0530

    add authentication

--- src/category/category-router.ts
+++ src/category/category-router.ts
@@ -5,6 +5,8 @@ import { CategoryService } from "./category-service";
 import logger from "../config/logger";
 import { asyncWrapper } from "../common/utils/wrapper";
 import authenticate from "../common/middlewares/authenticate";
+import { Roles } from "../common/constants";
+import { canAccess } from "../common/middlewares/canAccess";
 
 const router = express.Router();
 
@@ -15,6 +17,7 @@ const categoryController = new CategoryController(categorySerivce, logger);
 router.post(
     "/",
     authenticate,
+    canAccess([Roles.ADMIN]),
     categoryValidator,
     asyncWrapper(categoryController.create),
 );
