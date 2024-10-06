44a58935511ce8e3cf299aaf27608ef655d2285b
jabin-v <jabinv03@gmail.com>
2024-10-06 12:05:53 +0530

    add create topping

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,9 @@ import express, { Request, Response } from "express";
 import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 import categoryRouter from "./category/category-router";
 import productRouter from "./product/product-router";
+import toppingRouter from "./topping/topping-router";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
@@ -12,8 +14,25 @@ app.get("/", (req: Request, res: Response) => {
     res.json({ message: "hello from catalog service" });
 });
 
+// app.use(
+//     cors({
+//         //todo:move to .env file
+//         origin: ["http://localhost:5173"],
+//         credentials: true,
+//     }),
+// );
+
+app.use(
+    cors({
+        //todo:move to .env file
+        origin: ["http://localhost:5173", "http://localhost:3000"],
+        credentials: true,
+    }),
+);
+
 app.use("/categories", categoryRouter);
 app.use("/products", productRouter);
+app.use("/toppings", toppingRouter);
 
 app.use(globalErrorHandler);
 
